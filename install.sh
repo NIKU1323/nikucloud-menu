48bff5da17a27a0e2ac3314bd64673974343dd9b
NIKU1323 <puputjaya883@gmail.com>
2025-06-20 18:34:44 +0700

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "📥 Mengunduh script menu dari GitHub..."
-wget -O /usr/local/bin/menu https://raw.githubusercontent.com/NIKU1323/nikucloud-menu/main/menu
-chmod +x /usr/local/bin/menu
-echo "✅ Selesai! Ketik 'menu' untuk menjalankan."
-menu
